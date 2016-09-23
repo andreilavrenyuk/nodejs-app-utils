5c0273bbbf5923d2abbb30ca3b468267667de88c
Andrei Lavrenyuk <andrei.lavrenyuk@gmail.com>
2016-09-23 11:10:12 +0300

    add token util

---
+++ test/token.spec.js
@@ -0,0 +1,17 @@
+
+const chai = require('chai');
+const appToken = require('../lib/token');
+
+describe('checking token:', () => {
+  it('Create and decode:', (done) => {
+    const id = '1234567890';
+    const token = appToken.createToken(id);
+    appToken.decodeToken(token)
+      .then((obj) => {
+        chai.expect(obj.iss).to.equal(id);
+        done();
+      })
+      .catch(done);
+  });
+
+});
